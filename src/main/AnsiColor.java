e679932b9d37bac18922c7d1c5a33426ec828fa7
victor ponz <victorponz@gmail.com>
2025-02-21 11:17:24 +0100

    Ansicolor

---
+++ src/main/AnsiColor.java
@@ -0,0 +1,17 @@
+public enum AnsiColor {
+    BLACK("\u001B[30m"),
+    WHITE("\u001B[37m"),
+    BLUE("\u001B[38;2;40;177;249m"),
+    RED("\u001B[38;2;255;100;100m"),
+    GREEN("\u001B[32m"),
+    RESET("\u001B[0m");
+
+    private final String ansicolor;
+
+    AnsiColor(String code){
+        this.ansicolor = code;
+    }
+    public String getCode(){
+        return this.ansicolor;
+    }
+}
